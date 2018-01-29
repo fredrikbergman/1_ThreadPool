96bb80a268babec4646e3655fb3cec827709c7e7
Fredrik B <fredrik.bergman@semcon.com>
2018-01-29 09:03:17 +0100

    init

---
+++ ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FibonacciCalculations = 10;
+
+            ManualResetEvent[] doneEvents = new ManualResetEvent[FibonacciCalculations];
+            Fibonacci[] fibArray = new Fibonacci[FibonacciCalculations];
+            Random r = new Random();
+
+            Console.WriteLine("Launching {0} tasks..", FibonacciCalculations);
+            for (int i = 0; i < FibonacciCalculations; i++)
+            {
+                doneEvents[i] = new ManualResetEvent(false);
+                Fibonacci f = new Fibonacci(r.Next(20, 40), doneEvents[i]);
+                fibArray[i] = f;
+                ThreadPool.QueueUserWorkItem(f.ThreadPoolCallback, i);
+            }
+
+            WaitHandle.WaitAll(doneEvents);
+            Console.WriteLine("All calculations are complete!");
+
+            for (int i = 0; i < FibonacciCalculations; i++)
+            {
+                Fibonacci f = fibArray[i];
+                Console.WriteLine("Fibonacci({0}) = {1}",f.N, f.FibOfN);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
